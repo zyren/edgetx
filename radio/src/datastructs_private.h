1bd0d569551d547437db8ecce0d3655652789503
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-03-19 17:00:50 +0100

    WiP: support for LSM6DSxx

--- radio/src/datastructs_private.h
+++ radio/src/datastructs_private.h
@@ -770,14 +770,6 @@ PACK(struct TrainerData {
   #define SPLASH_MODE int8_t splashMode:3
 #endif
 
-#if defined(PCBXLITES)
-  #define GYRO_FIELDS \
-    int8_t   gyroMax; \
-    int8_t   gyroOffset;
-#else
-  #define GYRO_FIELDS
-#endif
-
 #if defined(COLORLCD)
   #define EXTRA_GENERAL_FIELDS \
     CUST_ARRAY(sticksConfig, struct_sticksConfig, stick_name_valid); \
@@ -910,9 +902,12 @@ PACK(struct RadioData {
 
   char ownerRegistrationID[PXX2_LEN_REGISTRATION_ID];
 
-  GYRO_FIELDS
+  NOBACKUP(int8_t uartSampleMode:2); // See UartSampleModes
 
-  NOBACKUP(int8_t   uartSampleMode:2); // See UartSampleModes
+#if defined(IMU)
+  NOBACKUP(int8_t imuMax);
+  NOBACKUP(int8_t imuOffset);
+#endif
 });
 
 #undef SWITCHES_WARNING_DATA
